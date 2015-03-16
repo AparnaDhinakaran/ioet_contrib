a6bd6b92d1ba8a6cfac82481fcac891d20c066da
Aparna Dhinakaran <aparnadhinak@berkeley.edu>
2015-03-16 00:59:25 -0700

    smart fan code

--- app/smart_fan.lua
+++ app/smart_fan.lua
@@ -2,7 +2,6 @@ require "cord"
 require "storm" 
 
 print ("Button test ")
-
 function onconnect(state)
    if tmrhandle ~= nil then
        storm.os.cancel(tmrhandle)
@@ -16,13 +15,52 @@ function onconnect(state)
    end
 end
 
+storm.io.set_mode(storm.io.OUTPUT,  storm.io.D11)
+storm.io.set_mode(storm.io.OUTPUT,  storm.io.D10)
+storm.io.set_mode(storm.io.OUTPUT,  storm.io.D9)
 
+function fanControl(state)
+	storm.io.set(0, storm.io.D11)
+	storm.io.set(0, storm.io.D10)
+	storm.io.set(0, storm.io.D9)
+	if state == "low" then 
+		storm.io.set(1, storm.io.D9)
+	end
+	if state == "med" then 
+		storm.io.set(1, storm.io.D10)
+	end
+	if state == "high" then 
+		storm.io.set(1, storm.io.D11)
+	end 	
+end 
+	
+	
+fanControl("high")
 storm.bl.enable("unused", onconnect, function()
    local svc_handle = storm.bl.addservice(0x1337)
    char_handle = storm.bl.addcharacteristic(svc_handle, 0x1338, function(x)
        print ("received: ",x)
    end)
 end)
+
+
+-- set buttons as outputs
+-- storm.io.set_mode(storm.io.OUTPUT,  storm.io.D11)
+
+-- state = 1 
+-- storm.io.set(1, storm.io.D11)
+-- this is a hack, poll button state every 50ms
+--storm.os.invokePeriodically(3*storm.os.SECOND, function ()
+--    -- get button states
+--    if state == 0 then 
+--	storm.io.set(1, storm.io.D11)
+--	state = 1
+--    end 
+--    if state == 1 then
+--	storm.io.set(0, storm.io.D11)
+--   	state = 0
+--    end 
+--end)
 
 print ("End")
 cord.enter_loop()
